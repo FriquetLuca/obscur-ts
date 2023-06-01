09c3b4bb9fda9010f58f90dee9fa7f958a96f6b0
FriquetLuca <friquet.luca@gmail.com>
2023-06-01 07:47:23 +0200

    Clean up mac os build

--- src/app/api/login.ts
+++ src/app/api/login.ts
@@ -2,6 +2,11 @@ import bcrypt from "bcrypt";
 import prisma from "../../database/prisma";
 import type { LoginUser } from "../../server/models/user";
 import type { GetHandlerRequest, GetHandlerReply } from "../../server/middleware/middleware";
+import { APIConfig } from "../../server/router/loadRoute";
+
+export const config: APIConfig = {
+  handlerType: "GET"
+};
 
 export default async function Login(request: GetHandlerRequest, reply: GetHandlerReply) {
   const { username, password } = JSON.parse(request.body as string) as LoginUser;
@@ -9,8 +14,10 @@ export default async function Login(request: GetHandlerRequest, reply: GetHandle
   if(user) {
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if(isPasswordValid) {
-      request.session.set("isLoggedIn", true);
-      request.session.set("userId", user.id);
+      request.session.set("cookie", {
+        id: user.id,
+        logged: true
+      });
       reply.send({ message: "Logged in successfully." });
     }
   }
--- src/app/api/logout.ts
+++ src/app/api/logout.ts
@@ -1,7 +1,6 @@
 import type { GetHandlerReply, GetHandlerRequest } from "../../server/middleware/middleware";
 
 export default async function Logout(request: GetHandlerRequest, reply: GetHandlerReply) {
-  request.session.set("isLoggedIn", false);
-  request.session.set("userId", null);
+  request.session.set("cookie", null);
   reply.send({ message: "Logged out successfully." });
 }
